cd7d7075ac7cc0238c60def54efda47c69734dba
Yogeshwar Dayal Agarwal <yogeshmario102@gmail.com>
2021-11-03 23:44:16 +0530

    initial commit

---
+++ triangleArea.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 30, b = 40, c = 50;
+    cout << "Area of Right Angle Triangle ABC is : " << b * a / 2 << endl;
+    return 0;
+}
